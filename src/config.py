e52d05457804cad596103f67e608872dd88e266e
Jackson Barreto <contato@jacksonbarreto.com>
2025-01-24 18:36:09 +0000

    up

--- src/config.py
+++ src/config.py
@@ -1,11 +1,10 @@
-col_raw_result = "raw_result"
-
-columns_for_results = [
-    col_raw_result,
-]
-
+COL_RAW_RESULTS = "raw_result"
+COL_ASSESSMENT_DATETIME = "assessment_datetime"
+COL_IP = "ip"
+COL_FINAL_SCORE = "final_score"
+COL_GRADE = "grade"
 
 config = {
     "max_workers": 10,
-    "test_ssl_path": ['..','testssl.sh', 'testssl.sh'],
+    "test_ssl_path": ['..', 'testssl.sh', 'testssl.sh'],
 }
--- src/scanner/http.py
+++ src/scanner/http.py
@@ -9,17 +9,18 @@ import pandas as pd
 from uuid import uuid4
 
 from src import config
-from src.config import config, col_raw_result, columns_for_results
+from src.config import config, COL_ASSESSMENT_DATETIME, COL_IP, COL_FINAL_SCORE, COL_GRADE, COL_RAW_RESULTS
 
-errors = []
-results = []
+final_errors = []
+final_results = []
 lock = threading.Lock()
 
+
 def scan(file_path):
-    global errors
-    global results
-    errors = []
-    results = []
+    global final_errors
+    global final_results
+    final_errors = []
+    final_results = []
     df = pd.read_csv(file_path)
     filename = os.path.basename(file_path)
     country_code = filename[:2]
@@ -27,9 +28,8 @@ def scan(file_path):
     if url_column_name is None:
         raise ValueError(f"No 'url' column found in CSV ({filename}).")
 
-    create_columns_for_results(df)
-
-    with ThreadPoolExecutor(max_workers=config["max_workers"]) as executor:
+    max_workers = config.get("max_workers", 5)
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = [executor.submit(scan_row, row, url_column_name) for index, row in df.iterrows()]
         for future in futures:
             try:
@@ -37,20 +37,23 @@ def scan(file_path):
             except Exception as e:
                 print(f"Thread error in CSV ({filename}): {e}")
 
-    output_filename = f"{country_code}_https_scanner{'_errors_' if False else ''}.csv"
-    output_dir = os.path.join('.', 'src', 'data', "results")
-    output_path = os.path.join(output_dir, output_filename)
-    results_df = pd.DataFrame(results)
-    results_df.to_csv(output_path, index=False)
-    print(results_df.head())
+    if final_results:
+        save(final_results, country_code)
+    if final_errors:
+        save(final_errors, country_code, error=True)
+
 
 def scan_row(row, url_column_name):
     url = row[url_column_name]
     temp_file_path = os.path.join(tempfile.gettempdir(), f"{str(uuid4())}.json")
     test_ssl_path = os.path.join(*config["test_ssl_path"])
     try:
+        print(f"Scanning URL: {row[url_column_name]}")
         result = subprocess.run(
-            [test_ssl_path, '--assuming-http', '--ids-friendly', '--sneaky', '--jsonfile-pretty', temp_file_path,  url],
+            args=[
+                test_ssl_path, '--assuming-http', '--ids-friendly', '--sneaky', '--jsonfile-pretty',
+                temp_file_path, url
+            ],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
@@ -60,37 +63,28 @@ def scan_row(row, url_column_name):
         with open(temp_file_path, 'r') as json_file:
             result = extract_result(json.load(json_file))
         with lock:
-            results.append({**row.to_dict(), **result})
+            final_results.append({**row.to_dict(), **result})
 
     except Exception as e:
         print(f"Error scanning URL {url}: {e}")
         with lock:
-            errors.append({**row.to_dict(), 'error': str(e)})
+            final_errors.append({**row.to_dict(), 'error': str(e)})
 
     finally:
         if os.path.exists(temp_file_path):
             os.remove(temp_file_path)
 
 
-
-
-
-
-def create_columns_for_results(df):
-    for col in columns_for_results:
-        if col not in df.columns:
-            df[col] = None
-
 def extract_result(raw_json):
     result = {}
     scan_result = raw_json.get('scanResult', [])
     scan_result = scan_result[0] if isinstance(scan_result, list) and scan_result else {}
 
-    result.update({"assessment_datetime": pd.Timestamp.now()})
-    result.update({"ip": scan_result.get("ip", None)})
+    result.update({COL_ASSESSMENT_DATETIME: pd.Timestamp.now()})
+    result.update({COL_IP: scan_result.get("ip", None)})
     result.update({**extract_protocols(scan_result.get("protocols", []))})
     result.update({**extract_rating(scan_result.get("ratings", []))})
-    #result.update({"raw_result": json.dumps(scan_result)})
+    result.update({COL_RAW_RESULTS: json.dumps(scan_result)})
 
     return result
 
@@ -103,13 +97,35 @@ def extract_protocols(protocols):
             protocols_result.update({protocol_name: False if "not" in protocol.get("finding", "").lower() else True})
     return protocols_result
 
+
 def extract_rating(ratings):
     rating_result = {}
     for rating in ratings:
         id_rating = rating.get("id", None)
         if id_rating == "final_score":
-            rating_result.update({"final_score": rating.get("finding", None)})
+            rating_result.update({COL_FINAL_SCORE: rating.get("finding", None)})
         elif id_rating == "overall_grade":
-            rating_result.update({"overall_grade": rating.get("finding", None)})
-
+            rating_result.update({COL_GRADE: rating.get("finding", None)})
     return rating_result
+
+
+def save(data, country_code, error=False):
+    folder = 'errors' if error else 'results'
+    output_dir = os.path.join('.', 'src', 'data', folder)
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    filename = f"{country_code}_https_scanner{'_errors_' if error else ''}.csv"
+    output_file = os.path.join(output_dir, filename)
+
+    if isinstance(data, list):
+        df = pd.DataFrame(data)
+    else:
+        df = data
+
+    if not df.empty:
+        if os.path.exists(output_file):
+            df.to_csv(output_file, mode='a', header=False, index=False)
+        else:
+            df.to_csv(output_file, index=False)
+    return output_file
